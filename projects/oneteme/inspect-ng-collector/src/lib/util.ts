f7c82a4d15a81fdff718c5ca1c0d9ab96f0d8a0c
YoussefDahi <youssefdahi686@gmail.com>
2024-07-16 17:24:22 +0200

    edit logInspect / durationformat

--- projects/oneteme/inspect-ng-collector/src/lib/util.ts
+++ projects/oneteme/inspect-ng-collector/src/lib/util.ts
@@ -17,6 +17,9 @@ export function logInspect(...args: any[]){
   if(WIN["inspect"]?.debug){
     if(args.length == 1 && typeof args[0] == 'function'){
       args = args[0]();
+      if(!Array.isArray(args)){
+        args = [args];
+      }
     }
     console.log('[INSPECT]', ...args);
   }
@@ -30,9 +33,9 @@ export function prettySessionFormat(session: MainSession){
     if(session.location){
       s+= `(${session.location}) `
     }
-    s+=prettyDurationFormat(session.start,session.end!);
+    s+=prettyDurationFormat(session.start,session.end!)+'\n';
     session.restRequests.forEach(r => {
-      s+= prettyRestRequestFormat(r);
+      s+= prettyRestRequestFormat(r)+'\n';
     })
   return s;
 }
@@ -42,30 +45,24 @@ function prettyRestRequestFormat(rest: RestRequest){
   if(rest.protocol){
     s+= `${rest.protocol}://`
   }
-
   if(rest.host){
     s+= rest.host
   }
-
   if(rest.port > 0){
     s+= `:${rest.port}`
   }
-
   if(rest.path){
     if(!rest.path.startsWith("/") && !s.endsWith("/")){
       s+= '/'
     }
     s+= rest.path;
   }
-
   if(rest.query){
     s+= rest.query
   }
-
   if(rest.exception?.type){
     s+= ` ${rest.exception?.type}:`;
   }
-
   if(rest.exception?.message){
     s+= ` ${rest.exception.message}`;
   }
@@ -75,7 +72,7 @@ function prettyRestRequestFormat(rest: RestRequest){
 }
 
 function prettyDurationFormat(start:number,end:number){
-    return  start && end ? `(in ${ (end - start) * 1000 } ms) \n` : '';
+    return  start && end ? `(in ${ (end - start).toFixed(2) } ms)` : '';
 }
 
 
@@ -128,7 +125,6 @@ export function detectBrowser() {
       console.error(e);
   }
   return undefined;
-
 }
 
 export function detectOs() {
