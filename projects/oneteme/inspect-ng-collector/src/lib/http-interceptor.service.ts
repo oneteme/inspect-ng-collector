5c3f375d7711c34bc52b97ee15a8d1b35fb44617
YoussefDahi <youssefdahi686@gmail.com>
2024-11-22 11:57:03 +0100

    vers ++

--- projects/oneteme/inspect-ng-collector/src/lib/http-interceptor.service.ts
+++ projects/oneteme/inspect-ng-collector/src/lib/http-interceptor.service.ts
@@ -24,7 +24,6 @@ export class HttpInterceptorService implements HttpInterceptor {
                 }
             },
             error => {
-                console.log("errorHandler", error);
                 id = getReqid(error.headers);
                 status = +error.status;
                 exception = {
